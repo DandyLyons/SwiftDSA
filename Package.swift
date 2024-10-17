34b9226bc2e426169eeeadd816c52d5a96662885
Daniel Lyons <72824209+DandyLyons@users.noreply.github.com>
2024-10-17 09:47:39 -0600

    Add CustomDump and IssueReporting dependencies

--- Package.swift
+++ Package.swift
@@ -15,16 +15,33 @@ let package = Package(
             targets: ["SwiftDSA"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ordo-one/package-benchmark.git", from: "1.25.0")
+        .package(url: "https://github.com/ordo-one/package-benchmark.git", from: "1.25.0"),
+        .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.0.0"),
+//        .package(url: "https://github.com/pointfreeco/swift-issue-reporting.git", from: "1.4.2"),
+// As of 2024-10-16 there is a naming discrepancy that renders SPM in Xcode unable to resolve SwiftIssueReporting.
+// This is because XCTestDynamicOverlay was renamed to SwiftIssueReporting.
+// The only way I've found to import SwiftIssueReporting here is to depend on swift-custom-dump and import SwiftIssueReporting transitively through the swift-custom-dump package. 
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftDSA"),
+            name: "SwiftDSA",
+            dependencies: [
+                .product(name: "CustomDump", package: "swift-custom-dump"),
+            ],
+            swiftSettings: [
+                .swiftLanguageMode(.v6)
+            ]
+        ),
         .testTarget(
             name: "SwiftDSATests",
-            dependencies: ["SwiftDSA"]
+            dependencies: [
+                "SwiftDSA",
+                .product(name: "CustomDump", package: "swift-custom-dump"),
+//                .product(name: "IssueReporting", package: "swift-issue-reporting"),
+            ],
+            swiftSettings: [
+                .swiftLanguageMode(.v6)
+            ]
         ),
         .executableTarget(
             name: "SwiftDSABenchmark",
